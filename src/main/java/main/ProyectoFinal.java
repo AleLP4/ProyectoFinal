2f4ba66b9b4f9973f56d536f464ebd5fa84d4c85
AleLP4 <alelopezpalma@gmail.com>
2025-10-17 15:38:39 -0600

    Primer commit

---
+++ src/main/java/main/ProyectoFinal.java
@@ -0,0 +1,17 @@
+package main;
+
+import view.GUIinicio;
+
+/**
+ *
+ * @author TI-IN
+ */
+public class ProyectoFinal {
+
+    public static void main(String[] args) 
+    {
+        GUIinicio demo = new GUIinicio();
+        demo.setVisible(true);
+        demo.setLocationRelativeTo(null);
+    }
+}
